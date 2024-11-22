2c71b557019f2932f8e19ac96133f3c6f393609b
Dhairya Khanna <dk9582671597@gmail.com>
2024-11-22 23:22:02 +0530

    feat: setting up router

--- api/api.go
+++ api/api.go
@@ -1,5 +1,27 @@
 package api
-import ("net/http")
+
+import (
+	"fmt"
+	"net/http"
+)
 type PizzaServer struct{
 	http.Handler
+}
+func NewPizzaServer()*PizzaServer{
+	p:=new(PizzaServer)
+	router:=http.NewServeMux()
+	router.Handle("/api/v1/player",http.HandlerFunc(p.playersHandler))
+	p.Handler = router
+	return p
+}
+func (p *PizzaServer) playersHandler(w http.ResponseWriter,r *http.Request){
+	switch r.Method{
+	case http.MethodGet:
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, `{"message": "Hello, World!"}`)
+	case http.MethodPost:
+		w.WriteHeader(http.StatusOK)
+	}
+
 }
--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/Dhairya3124/PizzaTime-Backend-Golang/api"
 )
 func main(){
-	if err := http.ListenAndServe(":5000", nil); err != nil {
+	p:=api.NewPizzaServer()
+	if err := http.ListenAndServe(":5000", p); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
 }
